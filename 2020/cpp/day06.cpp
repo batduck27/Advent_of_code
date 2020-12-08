e52fb3da1cb5c4a5720a870c2252fbaf7944c855
George Mocanu <mocanu.geo98@gmail.com>
2020-12-08 20:41:20 +0200

    Day 06 cpp & day 07

---
+++ 2020/cpp/day06.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <map>
+
+int main() {
+    std::ifstream fin("data.in");
+    long long sol1 = 0, sol2 = 0, x;
+    std::string str;
+
+    std::set<char> S;
+    std::map<char, int> M;
+    int lines = 0;
+
+
+    while (std::getline(fin, str)) {
+        lines ++;
+
+        if (str.empty()) {
+            sol1 += S.size();
+            S.clear();
+
+            for (const auto &[ch, r] : M) {
+                if (r == lines - 1) {
+                    sol2++;
+                }
+            }
+
+            M.clear();
+            lines = 0;
+            continue;
+        }
+
+        for (const auto & ch : str) {
+            S.insert(ch);
+            M[ch] ++;
+        }
+    }
+
+    sol1 += S.size();
+    S.clear();
+    for (const auto &[ch, r] : M) {
+        if (r == lines) {
+            sol2++;
+        }
+    }
+
+    M.clear();
+
+    std::cout << "The answer for part1 is: " << sol1 << "\n";
+    std::cout << "The answer for part2 is: " << sol2 << "\n";
+
+    fin.close();
+
+    return 0;
+}
---
+++ 2020/src/day07.rs
@@ -0,0 +1,95 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+use regex::Regex;
+use std::collections::{HashMap, VecDeque, HashSet};
+
+type Graph = HashMap<String, Vec<(String, usize)>>;
+
+fn get_input(filename: &str) -> Result<Vec<String>, Error> {
+    BufReader::new(File::open(filename).unwrap()).lines().collect()
+}
+
+fn process_input(input: Vec<String>) -> (Graph, Graph) {
+    let bag_reg = Regex::new(r"^(?:(\w+ \w+) bags contain)").unwrap();
+    let content_reg = Regex::new(r"(?:(\d+) (\w+ \w+) bags?[,\.])").unwrap();
+    let mut t_bags_graph = Graph::new();
+    let mut bags_graph = Graph::new();
+
+    for line in input {
+        let cur_bag = bag_reg.captures(&line).unwrap()[1].to_string();
+
+        for caps in content_reg.captures_iter(&line) {
+            t_bags_graph
+                .entry(caps[2].to_string())
+                .or_insert(Vec::new())
+                .push((cur_bag.clone(), caps[1].parse().unwrap()));
+
+            bags_graph
+                .entry(cur_bag.clone())
+                .or_insert(Vec::new())
+                .push((caps[2].to_string(), caps[1].parse().unwrap()));
+        }
+    }
+
+    (t_bags_graph, bags_graph)
+}
+
+fn solve_part1(graph: Graph) -> usize {
+    let mut Q: VecDeque<String> = VecDeque::new();
+    let mut vis: HashSet<String> = HashSet::new();
+
+    Q.push_back("shiny gold".to_string());
+
+    while !Q.is_empty() {
+        let node = Q.pop_front().unwrap();
+
+        if !vis.contains(&node) {
+            vis.insert(node.clone());
+
+            for (neigh, _) in graph.get(&node).unwrap_or(&vec![]) {
+                if !vis.contains(neigh) {
+                    Q.push_back(neigh.to_string());
+                }
+            }
+        }
+    }
+
+    vis.len() - 1
+}
+
+fn solve_part2(graph: Graph) -> usize {
+    let mut Q: VecDeque<(String, usize)> = VecDeque::new();
+    let mut ans = 0;
+
+    Q.push_back(("shiny gold".to_string(), 1));
+
+    while !Q.is_empty() {
+        let (node, cur_val) = Q.pop_front().unwrap();
+
+        for (neigh, cost) in graph.get(&node).unwrap_or(&vec![]) {
+            Q.push_back((neigh.to_string(), cur_val * cost))
+        }
+
+        ans += cur_val
+    }
+
+    ans - 1
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("\n==================================== Day 02 ====================================\n");
+
+    let input : Vec<String> = match get_input("input/day07.in") {
+        Ok(content) => content,
+        Err(err) => return Err(err.into()),
+    };
+
+    let (t_graph, graph) = process_input(input);
+
+    println!("The answer for part1 is: {}", solve_part1(t_graph));
+    println!("The answer for part2 is: {}", solve_part2(graph));
+
+    println!("\n================================================================================\n");
+
+    Ok(())
+}
